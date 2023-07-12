ed1787f3ecbbe6b4d176cf7c4533b28f618a8685
Funny_Silkie <ban.grumble99rar@gmail.com>
2023-07-12 13:34:33 +0900

    init python project

---
+++ src/bayes-mapper/bayes-mapper.py
@@ -0,0 +1,21 @@
+from sys import argv
+
+
+class Program:
+    @classmethod
+    def main(cls, args: list[str]) -> int:
+        """main関数です。
+
+        Args:
+            args (list[str]): 引数
+
+        Returns:
+            int: Exit Code
+        """
+
+        return 0
+
+
+if __name__ == "__main__":
+    exit_code: int = Program.main(argv[1:])
+    exit(exit_code)
